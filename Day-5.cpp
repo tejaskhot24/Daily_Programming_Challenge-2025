7a74960ecf97906fee0bfe5de52dbbce95196de6
tejaskhot24 <tejaskhot699@gmail.com>
2025-08-19 21:54:37 +0530

    Add Day-5 code

---
+++ Day-5.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+
+vector<int> findLeaders(int arr[], int n) {
+    vector<int> leaders;
+    int maxFromRight = arr[n - 1];
+
+    leaders.push_back(maxFromRight);
+
+    for (int i = n - 2; i >= 0; i--) {
+        if (arr[i] >= maxFromRight) {
+            leaders.push_back(arr[i]);
+            maxFromRight = arr[i];
+        }
+    }
+
+    reverse(leaders.begin(), leaders.end());
+    return leaders;
+}
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[100]; 
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    vector<int> leaders = findLeaders(arr, n);
+
+    cout << "Leaders: ";
+    for (int i = 0; i < leaders.size(); i++) {
+        cout << leaders[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
