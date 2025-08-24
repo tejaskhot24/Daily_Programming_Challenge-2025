5be91d78bea58329f658a372d37fb5552481e303
tejaskhot24 <tejaskhot699@gmail.com>
2025-08-24 23:14:46 +0530

    Create Day-10.java
    Added Group Anagrams code

---
+++ Day-10.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class GroupAnagrams {
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> result = groupAnagrams(strs);
+
+      
+        for (List<String> group : result) {
+            System.out.println(group);
+        }
+    }
+}
